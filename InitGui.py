81b79ec0008867c5b6d74249ced51fdc8ae9abe4
DeepSOIC <vv.titov@gmail.com>
2016-05-23 00:45:24 +0300

    Version check

--- InitGui.py
+++ InitGui.py
@@ -1,2 +1,11 @@
-import pomObserver
-pomObserver.start()
+import FreeCAD as App
+
+rev_number = int(App.Version()[2].replace("(Git)",""))
+if rev_number >= 7633:
+    import pomObserver
+    pomObserver.start()
+else:
+    App.Console.PrintError("Part-o-magic requires FreeCAD at least v0.17.7633. Yours appears to have a rev.{rev}, which is less. Part-o-magic is disabled.\n".format(rev= str(rev_number)))
+
+
+
