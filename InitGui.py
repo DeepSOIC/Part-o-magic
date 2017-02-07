71b87df4dedb3d1a1a40a9303876ce20f408b5f0
DeepSOIC <vv.titov@gmail.com>
2017-02-07 23:38:56 +0300

    Part-o-magic becomes a WORKBENCH!

--- InitGui.py
+++ InitGui.py
@@ -1,3 +1,4 @@
+
 import FreeCAD as App
 
 rev_number = int(App.Version()[2].replace("(Git)",""))
@@ -9,6 +10,33 @@ if rev_number >= 9933:
         PartOMagic.Gui.Observer.start()
 else:
     App.Console.PrintError("Part-o-magic requires FreeCAD at least v0.17.9933. Yours appears to have a rev.{rev}, which is less. Part-o-magic is disabled.\n".format(rev= str(rev_number)))
+
+
+class PartOMagicWorkbench (Workbench):
+    MenuText = 'Part-o-magic'
+    ToolTip = "Part-o-magic: experimental group and Part and Body automation"
+
+    def __init__(self):
+        # Hack: obtain path to POM by loading a dummy Py module
+        import os
+        import PartOMagic
+        self.__class__.Icon = os.path.dirname(PartOMagic.__file__) + u"/Gui/PyResources/icons/PartOMagic.svg".replace("/", os.path.sep)
+
+    def Initialize(self):
+        import PartOMagic as POM
+        POM.importAll()
+        
+        cmdsNewContainers = ([]
+            + POM.Features.Module.exportedCommands
+        )
+        self.appendToolbar('POMContainers', cmdsNewContainers)
+        self.appendMenu('Part-o-Magic', cmdsNewContainers)
+        
+    def Activated(self):
+        pass
+
+
+Gui.addWorkbench(PartOMagicWorkbench())
 
 
 
