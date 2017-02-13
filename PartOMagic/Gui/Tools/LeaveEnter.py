3a540b374e95365a98b8f4e65faf97308624311c
DeepSOIC <vv.titov@gmail.com>
2017-02-13 12:58:45 +0300

    New command: Leave
    Leaves editing of a sketch, or closes task dialog, or leaves a container

--- PartOMagic/Gui/Tools/LeaveEnter.py
+++ PartOMagic/Gui/Tools/LeaveEnter.py
@@ -6,18 +6,18 @@ from PartOMagic.Gui.Utils import *
 from PartOMagic.Base import Containers
 
 class CommandEnter:
-    "Command to create Module feature"
+    "Command to enter a feature"
     def GetResources(self):
         from PartOMagic.Gui.Utils import getIconPath
         return {'Pixmap'  : getIconPath("Sketcher_EditSketch.svg"),
                 'MenuText': "Enter object",
                 'Accel': "",
-                'ToolTip': "Enter object. (activate a container, or open a sketch for editing)."}
+                'ToolTip': "Enter object. (activate a container, or open a sketch for editing)"}
         
     def RunOrTest(self, b_run):
         sel = Gui.Selection.getSelection()
         if len(sel)==0 :
-            raise CommandError("Enter Object", "Enter Object command. Please select an object to enter, first. It can be a container, or a sketch")
+            raise CommandError("Enter Object", "Enter Object command. Please select an object to enter, first. It can be a container, or a sketch.")
         elif len(sel)==1:
             sel = screen(sel[0])
             ac = Containers.activeContainer()
@@ -25,6 +25,7 @@ class CommandEnter:
                 if sel in Containers.getContainerChain(ac) + [ac]:
                     raise CommandError("Enter Object", "Already inside this object")
                 if b_run: Containers.setActiveContainer(sel)
+                if b_run: Gui.Selection.clearSelection()
             else:
                 cnt = Containers.getContainer(sel)
                 if ac is cnt:
@@ -41,8 +42,6 @@ class CommandEnter:
             msgError(err)
             
     def IsActive(self):
-        #return True #temporarily disabled. Fixes mysterious Module tip changes
-        
         if not App.ActiveDocument: return False
         try:
             self.RunOrTest(b_run= False)
@@ -57,6 +56,52 @@ class CommandEnter:
 if App.GuiUp:
     Gui.addCommand('PartOMagic_Enter',  CommandEnter())
 
+
+
+
+class CommandLeave:
+    "Command to leave editing or a container"
+    def GetResources(self):
+        from PartOMagic.Gui.Utils import getIconPath
+        return {'Pixmap'  : getIconPath("Sketcher_LeaveSketch.svg"),
+                'MenuText': "Leave object",
+                'Accel': "",
+                'ToolTip': "Leave object. (close sketch editing, or close task dialog, or leave a container).",
+                'CmdType': "ForEdit"}
+        
+    def RunOrTest(self, b_run):
+        if Gui.ActiveDocument.getInEdit() is not None:
+            if b_run: Gui.ActiveDocument.resetEdit()
+        elif Gui.Control.activeDialog():
+            if b_run: Gui.Control.closeDialog()
+        else:
+            ac = Containers.activeContainer()
+            if ac.isDerivedFrom("App::Document"):
+                raise CommandError("Leave Object", "Nothing to leave.")
+            if b_run: Containers.setActiveContainer(Containers.getContainer(ac))
+            if b_run: Gui.Selection.clearSelection()
+            if b_run: Gui.Selection.addSelection(ac)
+    
+    def Activated(self):
+        try:
+            self.RunOrTest(b_run= True)
+        except Exception as err:
+            msgError(err)
+            
+    def IsActive(self):
+        if not App.ActiveDocument: return False
+        try:
+            self.RunOrTest(b_run= False)
+            return True
+        except CommandError as err:
+            return False
+        except Exception as err:
+            App.Console.PrintError(repr(err))
+            return True
+            
+
+if App.GuiUp:
+    Gui.addCommand('PartOMagic_Leave',  CommandLeave())
 def exportedCommands():
-    return ['PartOMagic_Enter']
+    return ['PartOMagic_Enter', 'PartOMagic_Leave']
 
