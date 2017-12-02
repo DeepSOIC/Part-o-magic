451264a3e994df186f8c1b0583da8954ddd88de8
DeepSOIC <vv.titov@gmail.com>
2017-12-02 15:29:03 +0300

    GlobalToolbar: update to have straighten-view button

--- PartOMagic/Gui/GlobalToolbar.py
+++ PartOMagic/Gui/GlobalToolbar.py
@@ -2,21 +2,30 @@ print("Loading GlobalToolbar")
 
 from . import CommandCollection1
 from .Tools import LeaveEnter
+from .View import SnapView
 from PartOMagic.Features import PartDesign as POMPartDesign
 
 import FreeCAD as App
 
 def registerToolbar():
-    p = App.ParamGet("User parameter:BaseApp/Workbench/Global/Toolbar/PartOMagic")
+    p = App.ParamGet("User parameter:BaseApp/Workbench/Global/Toolbar/PartOMagic_2")
     p.SetString("Name", "Part-o-Magic global")
     p.SetString(CommandCollection1.exportedCommands()[0], "FreeCAD")
     p.SetString(LeaveEnter.commandEnter.command_name, "FreeCAD")
     p.SetString(LeaveEnter.commandLeave.command_name, "FreeCAD")
+    p.SetString(SnapView.commandSnapView.command_name, "FreeCAD")
+    
+    
     p.SetBool("Active", 1)
+    
+    #remove old version of the toolbar
+    p_t = App.ParamGet("User parameter:BaseApp/Workbench/Global/Toolbar")
+    if p_t.HasGroup('PartOMagic'):
+        p_t.RemGroup('PartOMagic')
     
 def isRegistered():
     p = App.ParamGet("User parameter:BaseApp/Workbench/Global/Toolbar")
-    return p.HasGroup("PartOMagic")
+    return p.HasGroup("PartOMagic_2")
     
 def registerPDToolbar():
     creating_anew = not isPDRegistered()
