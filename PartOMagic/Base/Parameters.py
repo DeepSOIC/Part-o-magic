f0e6d97e1f3f93376b56eff3cc98344c49cb3e5b
DeepSOIC <vv.titov@gmail.com>
2026-01-19 20:12:43 +0100

    Parameters: track changes, and subscribe feature

--- PartOMagic/Base/Parameters.py
+++ PartOMagic/Base/Parameters.py
@@ -1,15 +1,22 @@
 
 import FreeCAD as App
 
+from typing import Self, Callable
+
 class Parameter(object):
     path = "User parameter:BaseApp/Preferences/Mod/PartOMagic"
     param = ""
     type = ""
     default = None
     method_dict = {"Bool": ("GetBool", "SetBool")}
+    subscribers = None
+
+    def __init__(self):
+        self.subscribers = []
+
     def get(self):
         if not hasattr(self, "_value"):
-            return self.get_stored()
+            self._value = self.get_stored()
         return self._value
     
     def get_stored(self):
@@ -20,10 +27,14 @@ class Parameter(object):
 
     def set_volatile(self, val):
         "Changes parameter for part-o-magic, but doesn't write it to preferences. It will self-reset on restart."
+        old_val = self.get()
         self._value = val
+        if val != old_val:
+            for func in self.subscribers:
+                func(self, old_val, val)
     
     def reset_volatile(self):
-        del(self._value)
+        self.set_volatile(self.get_stored())
     
     def __bool__(self):
         # this is here mainly to prevent "if Parameters.Something: # missing .get()" cause hard-to-catch bugs
@@ -31,18 +42,31 @@ class Parameter(object):
             return self.get()
         else:
             raise NotImplementedError(f"Parameter {self.param} is not a boolean")
-
-# # sadly, this does not work... Would have been nice though
-# class Observer(object):
-#     def onChange(self, param_grp, param_name):
-#         print(f"PoM: {param_name} changed")
-#         from PartOMagic.Base import Parameters
-#         if hasattr(Parameters, param_name):
-#             getattr(Parameters, param_name).reset_volatile()
     
-#     def __init__(self, path):
-#         App.ParamGet(path).Attach(self)
-# _observer1 = Observer(Parameter.path)
+    def subscribe(self, func: Callable[[Self, any, any], None]):
+        if not func in self.subscribers:
+            self.subscribers.append(func)
+
+    def unsubscribe(self, func):
+        try:
+            self.subscribers.remove(func)
+        except ValueError:
+            return False
+        else:
+            return True
+
+# sadly, this does not work... Would have been nice though
+class Observer(object):
+    def onChange(self, param_grp, param_name):
+        print(f"PoM: {param_name} changed")
+        from PartOMagic.Base import Parameters
+        if hasattr(Parameters, param_name):
+            getattr(Parameters, param_name).reset_volatile()
+    
+    def __init__(self, path):
+        self.p = App.ParamGet(path) # p going out of scope causes the subscription to be lost =( so we are saving it
+        self.p.Attach(self)
+_observer1 = Observer(Parameter.path)
 
 class _paramEnableObserver(Parameter):
     "Sets if PartOMagic Observer is enabled (Observer sorts new objects to active containers,\n\
